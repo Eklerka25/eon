7c06dec7fe5416ee2fb1829bd622c329ce9216f7
Eklerka25 <134164161+Eklerka25@users.noreply.github.com>
2025-04-23 15:49:38 +0200

    typedefs

---
+++ src/kernel/include/drivers/typedefs.h
@@ -0,0 +1,13 @@
+#pragma once
+
+typedef unsigned int ut;
+
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned long u32;
+typedef unsigned long long u64;
+
+typedef char i8;
+typedef short i16;
+typedef long i32;
+typedef long long i64;
